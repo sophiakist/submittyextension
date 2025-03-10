f4f46a07b7b5f5b9f094e9468ab867462ae5d3f1
Sophia Kist <sophia.a.kist@gmail.com>
2025-03-10 14:30:32 -0700

    renamed folder

--- src/sidebarContent.ts
+++ src/sidebarContent.ts
@@ -3,11 +3,11 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export function getLoginHtml(context: vscode.ExtensionContext): string {
-    const filePath = path.join(context.extensionPath, 'src', 'webviews', 'login.html');
+    const filePath = path.join(context.extensionPath, 'src', 'sidebar', 'login.html');
     return fs.readFileSync(filePath, 'utf8');
 }
 
 export function getClassesHtml(context: vscode.ExtensionContext): string {
-    const filePath = path.join(context.extensionPath, 'src', 'webviews', 'classes.html');
+    const filePath = path.join(context.extensionPath, 'src', 'sidebar', 'classes.html');
     return fs.readFileSync(filePath, 'utf8');
 }
